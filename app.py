1b8eba099d14b5bc51b6812935513682f6ace9ae
theiskander <iskander.burlai@gmail.com>
2025-01-07 19:10:06 +0200

    The registration route has created

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, flash
 from extensions import db
+
+from models import User
+from forms import RegistrationForm
 
 app = Flask(__name__)
 
@@ -12,6 +15,18 @@ db.init_app(app)
 @app.route('/')
 def index():
     return "It works!"
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(name=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You have registered!', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
