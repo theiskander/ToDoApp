4e5b0f18d96da96b80e1cb7ddf83bfe3cd2da4a4
theiskander <iskander.burlai@gmail.com>
2025-01-09 18:28:05 +0200

    Created the Task model

--- models.py
+++ models.py
@@ -12,3 +12,11 @@ class User(db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.hash_pass, password)
+    
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    due_date = db.Column(db.DateTime, default=None, nullable=True)
+    is_completed = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
